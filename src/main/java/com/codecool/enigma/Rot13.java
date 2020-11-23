81366a09648074813ad90b99c09b3f8acbf66812
pusztaizoltan <a.pusztaizoltan@gmail.com>
2020-11-23 19:56:43 +0100

    add Rot13

--- src/main/java/com/codecool/enigma/Rot13.java
+++ src/main/java/com/codecool/enigma/Rot13.java
@@ -2,14 +2,29 @@ package com.codecool.enigma;
 
 class Rot13 implements Cipher {
 
+//    abcdefghijklm nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ
+//    nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ abcdefghijklm
+
     @Override
     public String encrypt(String message) {
-        return null;
+        String[] base = "abcdefghijklm nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ".split("");
+        String[] coded = "nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ abcdefghijklm".split("");
+        String encryptedMessage = "";
+        for (int i = 0; i < base.length; i++) {
+            encryptedMessage = message.replaceAll(base[i], coded[i]);
+        }
+        return encryptedMessage;
     }
 
     @Override
-    public String decrypt(String message) {
-        return null;
+    public String decrypt(String message){
+        String[] coded = "abcdefghijklm nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ".split("");
+        String[] base = "nopqrstuvwxyz ABCDEFGHIJKLM NOPQRSTUVWXYZ abcdefghijklm".split("");
+        String decryptedMessage = "";
+        for(int i = 0; i<base.length;i++){
+            decryptedMessage = message.replaceAll(base[i], coded[i]);
+        }
+        return decryptedMessage;
     }
 
     private char encryptLetter(char letter) {
@@ -19,5 +34,6 @@ class Rot13 implements Cipher {
     private char decryptLetter(char letter) {
         return letter;
     }
-
 }
+
+
